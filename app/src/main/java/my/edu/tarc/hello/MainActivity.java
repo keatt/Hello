c2806250839fd791674142a3f87b490c60761661
keat <laubk-pa16@student.tarc.edu.my>
2018-10-16 13:06:40 +0800

    initial

---
+++ app/src/main/java/my/edu/tarc/hello/MainActivity.java
@@ -0,0 +1,33 @@
+package my.edu.tarc.hello;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //onCreate = main() function
+        super.onCreate(savedInstanceState);
+        //This method creates the UI
+
+        //R = resource class
+        //layout = design of UI (a folder)
+        setContentView(R.layout.activity_main);//manage by R
+
+
+
+
+    }
+    public void showMessage(View view){
+
+        //Link UI to the program
+        //class  instance
+        TextView textViewMessage; //TextView is a class
+        //textViewMessage is an instance
+        textViewMessage = findViewById(R.id.textViewMessage);
+        textViewMessage.setText("Hello, <your name here>");
+    }
+}
